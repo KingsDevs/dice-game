13ca6266f30b122e65dfc01fb9d3ba55e95edf8f
KingsDevs <sunnyckair@gmail.com>
2022-08-16 20:41:08 +0800

    implements Initializable in main controller

--- src/main/java/com/dc/MainController.java
+++ src/main/java/com/dc/MainController.java
@@ -1,11 +1,15 @@
 package com.dc;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
-public class MainController
+public class MainController implements Initializable
 {
 
     @FXML
@@ -17,10 +21,17 @@ public class MainController
     @FXML
     private ImageView dieImageView2;
 
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) 
+    {
+        
+        
+    }
+    
     @FXML
     void roll(ActionEvent event) 
     {
 
     }
-
 }
