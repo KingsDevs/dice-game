497dfbb4001b9984946ddf4139b63dab3bef133a
KingsDevs <sunnyckair@gmail.com>
2022-08-16 20:37:05 +0800

    added main controller

---
+++ src/main/java/com/dc/MainController.java
@@ -0,0 +1,26 @@
+package com.dc;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class MainController
+{
+
+    @FXML
+    private TextField betTextField;
+
+    @FXML
+    private ImageView dieImageView1;
+
+    @FXML
+    private ImageView dieImageView2;
+
+    @FXML
+    void roll(ActionEvent event) 
+    {
+
+    }
+
+}
