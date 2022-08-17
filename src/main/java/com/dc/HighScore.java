b8ffc37756676ef12bef92e4e071d64f9436d3c5
KingsDevs <sunnyckair@gmail.com>
2022-08-18 03:08:17 +0800

    added high score class

--- src/main/java/com/dc/HighScore.java
+++ src/main/java/com/dc/HighScore.java
@@ -2,11 +2,13 @@ package com.dc;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.channels.ScatteringByteChannel;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class HighScore 
+public class HighScore implements Comparable<HighScore>
 {
     private String playerName;
     private int moneyLeft;    
@@ -17,12 +19,30 @@ public class HighScore
         this.moneyLeft = moneyLeft;
     }
 
-    public static ArrayList<HighScore> getHighscores()
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    public Integer getMoneyLeft()
+    {
+        return moneyLeft;
+    }
+    
+    private static String getHighscorePath() throws IOException
+    {
+        String path = new File(".").getCanonicalPath();
+        path += "/hs.txt";
+
+        return path;
+    }
+
+    public static ArrayList<HighScore> getHighscores() throws IOException
     {
         ArrayList<HighScore> highScores = new ArrayList<HighScore>();
 
         try {
-            File highScoreFile = new File("hs.txt");
+            File highScoreFile = new File(getHighscorePath());
             Scanner scanner = new Scanner(highScoreFile);
 
             while(scanner.hasNextLine())
@@ -39,11 +59,30 @@ public class HighScore
         }
         
         return highScores;
-    } 
+    }
+    
+    public static void setHighScores(ArrayList<HighScore> highScores) throws IOException
+    {
+        Collections.sort(highScores, Collections.reverseOrder());
+
+        FileWriter fileWriter = new FileWriter(getHighscorePath());
+        
+        for(int i = 0; i < highScores.size(); i++)
+            fileWriter.write(highScores.get(i).getPlayerName() + "," + highScores.get(i).getMoneyLeft());
+
+        fileWriter.close();
+    }
 
     @Override
     public String toString()
     {
         return playerName + " - " + moneyLeft;
     }
+
+    @Override
+    public int compareTo(HighScore o) {
+        return moneyLeft - o.getMoneyLeft();
+    }
+
+    
 }
