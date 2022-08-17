0bbc33aa4af37e770ff286f37fd702b7d894ff72
KingsDevs <sunnyckair@gmail.com>
2022-08-18 01:15:26 +0800

    done with listview

--- src/main/java/com/dc/GameOverController.java
+++ src/main/java/com/dc/GameOverController.java
@@ -1,15 +1,14 @@
 package com.dc;
 
-import javafx.animation.FadeTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Duration;
 
-public class GameOverController 
+
+public class GameOverController
 {
 
     @FXML
@@ -29,14 +28,7 @@ public class GameOverController
     public void setMainController(MainController mainController)
     {
         this.mainController = mainController;
-    }
-
-    public void playEntrance()
-    {
-        FadeTransition fadeTransition = new FadeTransition(Duration.millis(800), anchorPane);
-        fadeTransition.setFromValue(1);
-        fadeTransition.setToValue(.2);
-        fadeTransition.play();
+        gameResultListView.getItems().addAll(mainController.getBetList());
     }
 
     @FXML
