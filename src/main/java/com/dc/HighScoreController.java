d5276bac92e1fae87458e404a1e909bd0b8c8ff5
KingsDevs <sunnyckair@gmail.com>
2022-08-18 12:37:30 +0800

    done clear high score functionality

--- src/main/java/com/dc/HighScoreController.java
+++ src/main/java/com/dc/HighScoreController.java
@@ -3,6 +3,7 @@ package com.dc;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 
@@ -14,8 +15,16 @@ public class HighScoreController
 
     public void updateHighScoreListView() throws IOException
     {
+        highScoreListView.getItems().clear();
         ArrayList<HighScore> highScores = HighScore.getHighscores();
         highScoreListView.getItems().addAll(highScores);
+    }
+
+    @FXML
+    void clear(ActionEvent event) throws IOException 
+    {
+        HighScore.clearHighScores();
+        highScoreListView.getItems().clear();
     }
 
 }
